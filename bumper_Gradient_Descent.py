4999b942df92ebcdb3ed8e95f77b54e96b33faf1
kcwen11 <88735880+kcwen11@users.noreply.github.com>
2022-07-13 18:24:51 -0500

    Add files via upload
    Add option of change magnet grade within lens

--- bumper_Gradient_Descent.py
+++ bumper_Gradient_Descent.py
@@ -1,5 +1,5 @@
 import parallel_Gradient_Descent
-from KevinBumperClass_adjustable import Bumper
+from KevinBumperClass_grades import Bumper
 import numpy as np
 
 r_norm = 0.05  # constants I use so that the gradient descent parameters are all on similar scales
@@ -9,18 +9,22 @@ L_norm = 0.1
 phi_norm = 0.2
 st_norm = 0.02
 
+norms = np.array([0.25, 0.25, 0.1, 0.2, 0.25, 0.25, 0.02])
+mag_unc = 0.01 / np.sin(5 * np.pi / 12) * 0.0254
+width_to_r = np.tan(np.pi / 12) * 2
+
 
 def func(args):
-    l1, d1, L, phi, l2, d2, st = args
-    testBumper = Bumper(0.95233173 * 0.0254, l1 * len_norm, d1 * d_norm, L * L_norm, phi * phi_norm,
-                        0.71907843 * 0.0254, l2 * len_norm, d2 * d_norm, 500, start=st * st_norm, long_off=0.0,
-                        focus_off=-0.03, real_mag=(1 / 2 * 0.0254, 3 / 4 * 0.0254, 3 / 8 * 0.0254))
-    return testBumper.cost(size_cost=True)
+    l1, d1, L, phi, l2, d2, st = args * norms
+    testBumper = Bumper((0.5 * 0.0254 + mag_unc) / width_to_r, l1, d1, L, phi,
+                        (0.5 * 0.0254 + mag_unc) / width_to_r, l2, d2, 500, start=st, long_off=0.0,
+                        focus_off=-0.03, real_mag=(1 / 2 * 0.0254, 3 / 4 * 0.0254, 1 / 2 * 0.0254))
+    return testBumper.cost(size_cost=True, alignment_cost=False)
 
 
 def main():
-    xi = np.array([0.8382, 0.912, -0.265, 0.5, 0.4064, 0.34, -0.6])
-
+    xi_meters = np.array([0.19668369, 0.37725694, -0.03887307, 0.09760237, 0.09681795, 0.22661218, -0.0119319732])
+    xi = xi_meters / norms
     test_opt = parallel_Gradient_Descent.gradient_Descent(func, xi, 0.01, 20, Plot=True, disp=True)
     print(test_opt)
 
